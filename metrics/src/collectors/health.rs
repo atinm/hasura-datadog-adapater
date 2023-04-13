addd3f5e69de4606ddc109fe2b5263bca41e7365
Atin M <atinm.dev@gmail.com>
2023-04-13 09:54:16 -0400

    Use prefix for service_checks

--- metrics/src/collectors/health.rs
+++ metrics/src/collectors/health.rs
@@ -8,14 +8,14 @@ pub(crate) async fn check_health(cfg: &Configuration, dd: &Client) {
         Ok(v) => {
             if v.status() == reqwest::StatusCode::OK {
                 debug!("Healthcheck OK");
-                dd.service_check("health", datadog_statsd::client::ServiceCheckStatus::Ok, &None);
+                dd.service_check(format!("{}.{}", cfg.prefix, "health").as_str(), datadog_statsd::client::ServiceCheckStatus::Ok, &None);
             } else {
                 debug!("Healthcheck NOK");
-                dd.service_check("health", datadog_statsd::client::ServiceCheckStatus::Critical, &None);
+                dd.service_check(format!("{}.{}", cfg.prefix, "health").as_str(), datadog_statsd::client::ServiceCheckStatus::Critical, &None);
             }
         },
         Err(e) => {
-            dd.service_check("health", datadog_statsd::client::ServiceCheckStatus::Unknown, &None);
+            dd.service_check(format!("{}.{}", cfg.prefix, "health").as_str(), datadog_statsd::client::ServiceCheckStatus::Unknown, &None);
             dd.incr("errors_total", &Some(vec!("type:health")));
 
             warn!("Failed to collect health check {}", e);
